912f5d4ab78696ca5f6d94fc8c9075ac7295346d
David Claux <dclaux@users.noreply.github.com>
2017-11-20 09:53:19 -0800

    Move controls folder under nodejs

--- source/html/controls/built/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KEY_TAB = 9;
-exports.KEY_ENTER = 13;
-exports.KEY_ESCAPE = 27;
-exports.KEY_UP = 38;
-exports.KEY_DOWN = 40;
-//# sourceMappingURL=constants.js.map
--- source/html/controls/built/numberbox.js
+++
@@ -1,7 +0,0 @@
-function determineDecimalSeparator() {
-    var n = 1.1;
-    // var result = n.toLocaleString().substring(1, 2);
-    var result = /^1(.+)1$/.exec(n.toLocaleString())[1];
-    return n;
-}
-//# sourceMappingURL=numberbox.js.map
