298a8b56611a13b5e8c0f435577ab8b52e10e0ce
Yeongsu Han <yeohan@microsoft.com>
2018-03-14 17:55:21 +0900

    Trim trailing newline in attributed text

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRViewController.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRViewController.mm
@@ -248,6 +248,13 @@ using namespace AdaptiveCards;
                                   // Initializing NSMutableAttributedString for HTML rendering is very slow
                                   NSMutableAttributedString *content = [[NSMutableAttributedString alloc] initWithData:htmlData options:options documentAttributes:nil error:nil];
 
+                                 {
+                                     // Trim trailing newline
+                                     NSString *text = [content mutableString];
+                                     NSString* result = [text stringByReplacingOccurrencesOfString:@"\\n$" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, [text length])];
+                                     [[content mutableString] setString:result];
+                                 }
+
                                   __block ACRUILabel *lab = nil; // generate key for text map from TextBlock element's id
                                   NSString *key = [NSString stringWithCString:txtElem->GetId().c_str() encoding:[NSString defaultCStringEncoding]];
                                   // syncronize access to text map
@@ -278,7 +285,7 @@ using namespace AdaptiveCards;
                                                                NSParagraphStyleAttributeName:paragraphStyle,
                                                                NSForegroundColorAttributeName:[ACRTextBlockRenderer getTextBlockColor:txtElem->GetTextColor() colorsConfig:colorConfig subtleOption:txtElem->GetIsSubtle()],
                                                                }
-                                                       range:NSMakeRange(0, content.length - 1)];
+                                                       range:NSMakeRange(0, content.length)];
                                       lab.attributedText = content;
 
                                       // Shrink font size to fit all text in a single line
@@ -286,7 +293,6 @@ using namespace AdaptiveCards;
                                       if ([lab numberOfLines] == 1001)
                                       {
                                           [lab setNumberOfLines:1];
-                                          [lab setText:[[lab text] stringByReplacingOccurrencesOfString:@"\n" withString:@""]]; // Trim trailing line break
                                           [lab setAdjustsFontSizeToFitWidth:YES];
                                           [lab setLineBreakMode:NSLineBreakByTruncatingTail]; // trick to make setAdjustsFontSizeToFitWidth effective
                                       }
